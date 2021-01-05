b3e4694779385987e0c3ee95a22bc1c8f535f394
Randall Noval <randynov@gmail.com>
2021-01-05 18:13:15 -0500

    cleaning up lint issues

--- README.md
+++ README.md
@@ -1,17 +1,62 @@
-Run the command example: python scryfall.py inside the Randall_Noval folder on the command line
-must be installed python3+, openpyxl(pip install openpyxl), requests(pip install requests), pandas(pip install pandas),base64(pip install base64), selenium(pip install selenium), webdriver_manager(pip install webdriver-manager)
-You have to edit config.py:
-csv_file = 'cards-sample.csv' # fixed for repo
-github_username  # fixed for repo randynov
-github_repo_name  # fixed for repo mtgmanager2aetherhub
+# mtgmanager2aetherhub
+Populates MTG Manager CSV with Scryfall ID from Aetherhub
 
-branch_github_user_name  # The name of the account that will make a pull request example(tekin7 my github account name)
-branch_name  # branch name. change, if change your github account branch
-password= '' # The pasword of the account that will make a pull request
-message = "Update cards-sample" # commit and pull request message
+This script takes input from a CSV file in the following format:
+`"collector_number","extras","language","name","oracle_id","quantity","scryfall_id","set_code","set_name"`
 
-github_token = ''	# your github access token (login github->settings->Developer settings->personal access token->generate new token you can slect all scopes and generate token
+and with a minimum of Name and set_code filled in, searches the Scryfall API, populates the corresponding Scryfall ID and returns an updated CSV.
 
+
+## To Run
+`python scryfall.py`
+
+
+### Required Libraries
+* Python3+
+* openpyxl (pip3 install openpyxl)
+* requests (pip3 install requests)
+* pandas (pip3 install pandas)
+* base64 (pip3 install base64)
+* selenium (pip3 install selenium)
+* webdriver_manager (pip3 install webdriver-manager)
+
+Specify CSV file in the config.py:
+csv_file = 'cards-sample.csv'
+
+### Standards
 Written using the pep-8 standard
 
-Please do not forget to fork the main repo from the account for which you will make a pull request request.
+# EXAMPLES
+## Search for "Ashes to Ashes" from the "The Dark" set
+#### EXAMPLE INPUT
+`"","","en","Ashes to Ashes","","2","","DRK",""`
+
+#### API Call
+https://api.scryfall.com/cards/search?q=set%3Ddrk%2Bname%3DAshes%20to%20Ashes
+
+#### EXAMPLE OUTPUT
+`"","","en","Ashes to Ashes","","2","825496e5-19c7-4f50-8070-0265a58608dc","DRK",""`
+
+## Search for "Ashes to Ashes" from the "4th Edition" set
+#### EXAMPLE INPUT
+`"","","en","Ashes to Ashes","","2","","4ed",""`
+
+#### API Call
+https://api.scryfall.com/cards/search?q=set%3D4ed%2Bname%3DAshes%20to%20Ashes
+
+#### EXAMPLE OUTPUT
+`"","","en","Ashes to Ashes","","2","28f40650-9dd5-473d-a660-448672d475a5","4ED",""`
+
+## Search for "Archaeomancer" from the "Commander 2017" set
+#### EXAMPLE INPUT
+`"","","en","Archaeomancer","","2","","C17",""`
+
+#### API Call
+https://api.scryfall.com/cards/search?q=set%3Dc17%2Bname%3DArchaeomancer
+
+#### EXAMPLE OUTPUT
+`"","","en","Archaeomancer","","1","4a67be8d-5db6-42c3-8c89-f07bfbd2dd51","C17",""`
+
+# SUPPORTING DOCUMENTATION
+Uses the Scryfall Card Search API:
+https://scryfall.com/docs/api/cards/search
--- scryfall.py
+++ scryfall.py
@@ -1,10 +1,10 @@
 import requests
 from urllib.parse import urlencode
-import json 
+import json
 import logging
 import openpyxl
 import pandas as pd
-import csv   
+import csv
 from config import *
 import base64
 from selenium import webdriver
@@ -13,150 +13,180 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.keys import Keys
 import json
-  
+
+
 def scryfall():
-    df = pd.read_csv(csv_file, error_bad_lines=False).fillna("") # read csv file 
-    """" 
+    # read csv file
+    df = pd.read_csv(csv_file, error_bad_lines=False).fillna(
+        "")
+    """"
     Convert csv columns to dataframe with pandas
     """
-    collector_number   = df['collector_number']
-    extras             = df['extras']
-    language           = df['language']
-    name               = df['name']
-    oracle_id          = df['oracle_id']
-    quantitie          = df['quantity']
-    scryfall_id        = df['scryfall_id']
-    set_code           = df['set_code']
-    set_name           = df['set_name']
+    collector_number = df['collector_number']
+    extras = df['extras']
+    language = df['language']
+    name = df['name']
+    oracle_id = df['oracle_id']
+    quantitie = df['quantity']
+    scryfall_id = df['scryfall_id']
+    set_code = df['set_code']
+    set_name = df['set_name']
 
     data_count = df.shape[0]
     for i in range(data_count):
         if scryfall_id[i] == '':
             r, result = request_to_api(name[i], set_code[i])
-            if r.status_code == 200:    # if the status code is 200 the request is correct
-                total_cards = result['total_cards']     # card count
-                data = result['data']   # response data
-                
-                if total_cards > 1: # If it returns more than 1 result, the id of the result matching the card name is retrieved.
+            # if the status code is 200 the request is correct
+            if r.status_code == 200:
+                # card count
+                total_cards = result['total_cards']
+                # response data
+                data = result['data']
+
+                # If it returns more than 1 result, the id of the
+                # result matching the card name is retrieved.
+                if total_cards > 1:
                     for j in data:
                         if j['name'] == name[i]:
                             scryfall_id[i] = j['id']
                 else:
                     scryfall_id[i] = data[0]['id']
-                    
+
                     # logging info check cards_sample.log
                     params = 'info'
-                    message = 'Card found'+' {' + 'name: ' + name[i] + ', ' + 'set_code: '+ set_code[i] +', '+ 'id: '+ scryfall_id[i]
-                    save_log(message,params) # save_log function
+                    message = 'Card found' + \
+                        ' {' + 'name: ' + name[i] + ', ' + 'set_code: ' + \
+                        set_code[i] + ', ' + 'id: ' + scryfall_id[i]
+                    save_log(message, params)  # save_log function
                     print('Card found:', name[i])
             else:
                 # logging error check cards_sample.log
                 params = 'error'
-                message = 'Card not found'+' {' + 'name: ' + name[i] + ', ' + 'set_code: '+ set_code[i] +', ' + 'details: '+ result['details']
-                save_log(message,params) # save_log function
+                message = 'Card not found' + \
+                    ' {' + 'name: ' + name[i] + ', ' + 'set_code: ' + \
+                    set_code[i] + ', ' + 'details: ' + result['details']
+                save_log(message, params)  # save_log function
                 print('Card not found...')
-                
-    df.to_csv('cards-sample.csv', index=False, quotechar='"', quoting=csv.QUOTE_ALL)  # save updated csv             
-            
-            
 
-def request_to_api(name,set_code):
+    df.to_csv('cards-sample.csv', index=False, quotechar='"',
+              # save updated csv
+              quoting=csv.QUOTE_ALL)
+
+
+def request_to_api(name, set_code):
     """
     Request to scryfall api
     """
-    url = url_encode(name, set_code)     # url encode funtion
-    r = requests.get(url)   # request to scryfall api
-    result = json.loads(r.text)  # convert response to json
-    return r, result        
-            
-            
-def url_encode(name, set_code):    
+    # url encode funtion
+    url = url_encode(name, set_code)
+    # request to scryfall api
+    r = requests.get(url)
+    # convert response to json
+    result = json.loads(r.text)
+    return r, result
+
+
+def url_encode(name, set_code):
     """
     URL correction in scryfall api format
     """
-    base_endpoint = "https://api.scryfall.com/cards/search?q=" # base url
+    # base url
+    base_endpoint = "https://api.scryfall.com/cards/search?q="
+
     # query string parameters
     params = {
-         "set":set_code,
-         "name":name
+        "set": set_code,
+        "name": name
     }
-    params_encoded = urlencode(params).replace("+","%20").replace("=","%3D").replace("&","%2B") # url encoded join parameters
+
+    # url encoded join parameters
+    params_encoded = urlencode(params).replace(
+        "+", "%20").replace("=", "%3D").replace("&", "%2B")
     detail_url = f"{base_endpoint}{params_encoded}"
     return detail_url
-  
+
 
 def save_log(message, params):
     """
-    Save log information and error info 
+    Save log information and error info
     """
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO) #set log level
-    
+    # set log level
+    logger.setLevel(logging.INFO)
+
     # create file handler
     handler = logging.FileHandler('cards_sample.log')
     handler.setLevel(logging.INFO)
-    
+
     # create logging format
     formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
 
     # save handler to logger
     logger.addHandler(handler)
-    
-    logger.info(message) if params == 'info' else  logger.error(message)
- #%%      
+
+    logger.info(message) if params == 'info' else logger.error(message)
+    # %%
+
+
+"""
 def github_commit():
-    """
-    github commit
-    """
-    url = f'https://api.github.com/repos/{branch_github_user_name}/{github_repo_name}/contents/{csv_file}?access_token=' +github_token
+    url = f'https://api.github.com/repos/{branch_github_user_name}/{github_repo_name}/contents/{csv_file}?access_token=' + github_token
     r = requests.get(url)
-    sha = r.json()['sha'] # target repo sha 
-    
+    # target repo sha
+    sha = r.json()['sha']
+
     with open(csv_file, 'rb') as f:
         content = str(base64.b64encode(f.read()), encoding='utf8')
-        data = {'message': message, 'content': content, 'sha': sha, 'branch': 'main'}
+        data = {'message': message, 'content': content,
+                'sha': sha, 'branch': 'main'}
         r_to_github = requests.put(url, json=data)
-        
+
 def github_pull_request():
-    """
-    github changes pull request to
-    """
+    # github changes pull request to
     browserProfile = webdriver.FirefoxProfile()
     options = FirefoxOptions()
     browserProfile.set_preference("intl.accept_languages", "en-us")
     options.add_argument("--headless")
-    browserProfile.set_preference("general.useragent.override","Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166")
-    browser = webdriver.Firefox(options=
-    options,firefox_profile=browserProfile, executable_path=GeckoDriverManager().install())
-    
+    browserProfile.set_preference(
+        "general.useragent.override", "Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; Microsoft; Lumia 640 XL LTE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Mobile Safari/537.36 Edge/12.10166")
+    browser = webdriver.Firefox(options=options, firefox_profile=browserProfile,
+                                executable_path=GeckoDriverManager().install())
+
     browser.get("https://github.com/login")
     sleep(2)
     # Github login process
     username_input = browser.find_element_by_id("login_field")
     password_input = browser.find_element_by_id("password")
-    
+
     username_input.send_keys(branch_github_user_name)
     password_input.send_keys(password)
     password_input.send_keys(Keys.ENTER)
     sleep(2)
     # redirect pull request page
-    browser.get(f"https://github.com/{branch_github_user_name}/{github_repo_name}/pulls")
+    browser.get(
+        f"https://github.com/{branch_github_user_name}/{github_repo_name}/pulls")
     sleep(2)
-    browser.get(f"https://github.com/{github_username}/{github_repo_name}/compare/{branch_name}...{branch_github_user_name}:main")
-    
-    sleep(2)
-    try:
-        browser.find_element_by_xpath("/html/body/div[4]/div/main/div[2]/div/div[4]/div/button").click()
-    except:
-        browser.find_element_by_xpath("/html/body/div[4]/div/main/div[2]/div/div[4]/div/button").click()
+    browser.get(
+        f"https://github.com/{github_username}/{github_repo_name}/compare/{branch_name}...{branch_github_user_name}:main")
+
     sleep(2)
     try:
-        browser.find_element_by_xpath("//*[@id='new_pull_request']/div/div[1]/div/div[2]/div[2]/div/button").click()
+        browser.find_element_by_xpath(
+            "/html/body/div[4]/div/main/div[2]/div/div[4]/div/button").click()
     except:
-        browser.find_element_by_xpath("//*[@id='new_pull_request']/div/div[1]/div/div[2]/div[2]/div/button").click()
+        browser.find_element_by_xpath(
+            "/html/body/div[4]/div/main/div[2]/div/div[4]/div/button").click()
+    sleep(2)
+    try:
+        browser.find_element_by_xpath(
+            "//*[@id='new_pull_request']/div/div[1]/div/div[2]/div[2]/div/button").click()
+    except:
+        browser.find_element_by_xpath(
+            "//*[@id='new_pull_request']/div/div[1]/div/div[2]/div[2]/div/button").click()
 
 
 if __name__ == '__main__':
     scryfall()
     github_commit()
+"""
